0d4378e12ff09787e1dd3efc536dec9260a8219b
Sumair-Idrak1 <sumair@idrakai.com>
2025-06-27 20:09:10 +0500

    updation

---
+++ Range_function/Python_range_function.py
@@ -0,0 +1,18 @@
+#Output will be 0-9
+
+#for i in range(10):
+#   print(i)
+
+
+#using range with two arguments
+#for i in range(2,10):
+#    print(i)
+
+
+#3 arguments
+#for i in range(2,10,2):
+#    print(i)
+
+
+#y_list=list(range(0,100,2))
+#mnt(my_list)
---
+++ math_operations/Package.py
@@ -0,0 +1,6 @@
+import math_operations.addition as math_1
+import math_operations.subtraction as math_2
+
+
+print(math_1.add(5,6))
+print(math_2.subtract(5,6))
